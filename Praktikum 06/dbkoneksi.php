5a0dd51069d7ecdf25133f3e9fb4093b7d949d4c
Madyanwar <146082542+Madyanwar@users.noreply.github.com>
2024-03-26 10:03:33 +0700

    Add files via upload

---
+++ Praktikum 06/dbkoneksi.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$dbname = "dbpuskesmas";
+$user = "root";
+$pass = "";
+
+$dsn = "mysql:host=$host;port=3307;dbname=$dbname";
+
+$dbh = new PDO($dsn, $user, $pass);
+
+?>
